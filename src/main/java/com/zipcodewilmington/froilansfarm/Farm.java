3a432dd2c5a28abd6d037be7db9b7e61610dd2aa
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-05-28 19:49:39 -0400

    added things

--- src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -1,13 +1,41 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Farm {
     private Field field;
+    private FarmHouse farmHouse;
+    private List<Stable> stables;
 
     public Farm() {
         field = new Field();
+        farmHouse = new FarmHouse();
+        stables = new ArrayList<>();
+        stables.add(new Stable());
+        stables.add(new Stable());
+        stables.add(new Stable());
+        stables.get(0).addHorse(new Horse("Horse1"));
+        stables.get(0).addHorse(new Horse("Horse2"));
+        stables.get(0).addHorse(new Horse("Horse3"));
+        stables.get(1).addHorse(new Horse("Horse4"));
+        stables.get(1).addHorse(new Horse("Horse5"));
+        stables.get(1).addHorse(new Horse("Horse6"));
+        stables.get(2).addHorse(new Horse("Horse7"));
+        stables.get(2).addHorse(new Horse("Horse8"));
+        stables.get(2).addHorse(new Horse("Horse9"));
+        stables.get(2).addHorse(new Horse("Horse10"));
+    }
+
+    public FarmHouse getFarmHouse() {
+        return farmHouse;
     }
 
     public Field getField() {
         return field;
+    }
+
+    public List<Stable> getStables() {
+        return stables;
     }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/FarmTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/FarmTest.java
@@ -1,8 +1,8 @@
 package com.zipcodewilmington.froilansfarm;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class FarmTest {
 
@@ -11,5 +11,44 @@ public class FarmTest {
         Farm farm = new Farm();
 
         assertNotNull(farm.getField());
+    }
+
+    @Test
+    void farmHasThreeStables() {
+        Farm farm = new Farm();
+
+        assertEquals(3, farm.getStables().size());
+    }
+
+    @Test
+    void farmHasTenHorses() {
+        Farm farm = new Farm();
+        int horseCount = 0;
+        for (Stable stable : farm.getStables()) {
+            horseCount += stable.getHorses().size();
+        }
+        assertEquals(10, horseCount);
+    }
+
+    @Test
+    void farmHasFourChickenCoops() {
+        Farm farm = new Farm();
+        assertEquals(4, farm.getChickenCoops().size());
+    }
+
+    @Test
+    void farmHasFifteenChickens() {
+        Farm farm = new Farm();
+        int chickenCount = 0;
+        for (ChickenCoop coop : farm.getChickenCoops()) {
+            chickenCount += coop.getChickens().size();
+        }
+        assertEquals(15, chickenCount);
+    }
+
+    @Test
+    void farmHasOneFarmHouse() {
+        Farm farm = new Farm();
+        assertNotNull(farm.getFarmHouse());
     }
 }
