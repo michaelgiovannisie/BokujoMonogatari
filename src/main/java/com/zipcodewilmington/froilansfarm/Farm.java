3c13f978daf4fcbcb1e9a07380077f6d85cf7b50
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-05-28 16:58:11 -0400

    add again

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -0,0 +1,13 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Farm {
+    private Field field;
+
+    public Farm() {
+        field = new field();
+    }
+
+    public Field getFiel() {
+        return field;
+    }
+}
