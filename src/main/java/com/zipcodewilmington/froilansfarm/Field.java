9ae01ab524c2c412a02ef331f48bf4b13165acf1
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-05-28 21:30:14 -0400

    updated

--- src/main/java/com/zipcodewilmington/froilansfarm/Field.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Field.java
@@ -1,9 +1,18 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Field {
     private List<CropRow> cropRows;
+
+    public Field() {
+        cropRows = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            cropRows.add(new CropRow());
+        }
+    }
 
     public List<CropRow> getCropRows() {
         return cropRows;
