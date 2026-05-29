8ca6407901265d0eec9928115e38fc85c5bea19c
michaelgiovannisie <siemichaelgiovanni@gmail.com>
2026-05-28 22:34:43 -0400

    added more test

--- src/test/java/com/zipcodewilmington/froilansfarm/WednesdayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/WednesdayTest.java
@@ -24,4 +24,28 @@ public class WednesdayTest {
     void chickenCanMakeNoise() {
         assertDoesNotThrow(() -> chicken.makeNoise());
     }
+
+    @Test
+    void horseCanBeMountedAndDismounted() {
+        Rider rider = new Farmer("Froilan");
+        Horse horse = new Horse("Horse");
+        assertDoesNotThrow(() -> horse.beMounted(rider));
+        assertDoesNotThrow(() -> horse.beDismounted(rider));
+    }
+
+    @Test
+    void tractorCanBeMountedAndDismounted() {
+        Rider rider = new Farmer("Froilan");
+        Tractor tractor = new Tractor();
+        assertDoesNotThrow(() -> tractor.beMounted(rider));
+        assertDoesNotThrow(() -> tractor.beDismounted(rider));
+    }
+
+    @Test
+    void cropDusterCanBeMountedAndDismounted() {
+        Rider rider = new Pilot("Froilanda");
+        CropDuster cropDuster = new CropDuster();
+        assertDoesNotThrow(() -> cropDuster.beMounted(rider));
+        assertDoesNotThrow(() -> cropDuster.beDismounted(rider));
+    }
 }
